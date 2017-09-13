fffbb4d9ed89689123cf3764ff234752592f1a3e
JCS <joshsad2@gmail.com>
2017-09-13 08:12:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,8 @@ var playlist = new Object({
 })
 
 function updatePlaylist(playlist, artistName, songTitle) {
-
+  playlist[artistName] = songTitle;
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
